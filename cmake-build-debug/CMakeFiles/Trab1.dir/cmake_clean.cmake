d051dddf015eedd5275f16f29025161d37ea4ece
Matheus Rocha <theusrsilva@gmail.com>
2019-04-29 23:58:48 -0300

    getting started

---
+++ cmake-build-debug/CMakeFiles/Trab1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Trab1.dir/main.c.o"
+  "Trab1.pdb"
+  "Trab1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Trab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
