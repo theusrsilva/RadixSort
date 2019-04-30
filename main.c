181c957d2981b60f4d621ca3284dce4e2b90e72b
Matheus Rocha <theusrsilva@gmail.com>
2019-04-30 00:42:11 -0300

    adicionando numeros randomicos a um vetor cujo usuario escolhe o tamanho e o maior digito

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    int n;
+    double d;
+    srand(time(NULL));
+    printf("digite a quantidade de numeros que voce quer:\n");
+    scanf("%d",&n);
+    printf("digite a o maior digito presente:\n");
+    scanf("%lf",&d);
+    int v[n];
+    for(int j =0;j<n;j++){
+        v[j]=rand() % (int)pow(10.0,d);
+    }
+    for (int i=0;i<n;i++){
+        printf("%d\n",v[i]);
+    }
 }
