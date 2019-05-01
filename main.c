d00b3d34fc5e8635207f677faff55fbd34d4016d
Matheus Rocha <theusrsilva@gmail.com>
2019-05-01 12:14:17 -0300

    adicionado como transformar u int em string e depois voltar (esta comentado depois do main)

--- main.c
+++ main.c
@@ -2,54 +2,72 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <string.h>
 #include "fila.c"
 int main() {
-    int n;
-    double d;
-    srand(time(NULL));
-    printf("digite a quantidade de numeros que voce quer:\n");
-    scanf("%d",&n);
-    printf("digite a o maior digito presente:\n");
-    scanf("%lf",&d);
-    int vetorInicial[n];
-    for(int j =0;j<n;j++){
-        if(d==1){
-            vetorInicial[j]=rand() % (int)pow(10.0,d);
-        }else{
-            vetorInicial[j]=rand() % (int)pow(10.0,d);
-            while(vetorInicial[j]<(int)pow(10.0,d-1)){
-                vetorInicial[j]=rand() % (int)pow(10.0,d);
-            }
-        }
-    }
-    printf("tabela: ");
-    for (int i=0;i<n;i++){
-        printf("%d ",vetorInicial[i]);
-    }
+//    int n;
+//    double d;
+//    srand(time(NULL));
+//    printf("digite a quantidade de numeros que voce quer:\n");
+//    scanf("%d",&n);
+//    printf("digite a o maior digito presente:\n");
+//    scanf("%lf",&d);
+//    int vetorInicial[n];
+//    for(int j =0;j<n;j++){
+//        if(d==1){
+//            vetorInicial[j]=rand() % (int)pow(10.0,d);
+//        }else{
+//            vetorInicial[j]=rand() % (int)pow(10.0,d);
+//            while(vetorInicial[j]<(int)pow(10.0,d-1)){
+//                vetorInicial[j]=rand() % (int)pow(10.0,d);
+//            }
+//        }
+//    }
+//    printf("tabela: ");
+//    for (int i=0;i<n;i++){
+//        printf("%d ",vetorInicial[i]);
+//    }
+//
+//    fila* fila0;
+//    fila* fila1;
+//    fila* fila2;
+//    fila* fila3;
+//    fila* fila4;
+//    fila* fila5;
+//    fila* fila6;
+//    fila* fila7;
+//    fila* fila8;
+//    fila* fila9;
+//    fila0=inicializaFila();
+//    fila1=inicializaFila();
+//    fila2=inicializaFila();
+//    fila3=inicializaFila();
+//    fila4=inicializaFila();
+//    fila5=inicializaFila();
+//    fila6=inicializaFila();
+//    fila7=inicializaFila();
+//    fila8=inicializaFila();
+//    fila9=inicializaFila();
+//    fila* vetorRadix[10]={fila0,fila1,fila2,fila3,fila4,fila5,fila6,fila7,fila8,fila9};
 
-    fila* fila0;
-    fila* fila1;
-    fila* fila2;
-    fila* fila3;
-    fila* fila4;
-    fila* fila5;
-    fila* fila6;
-    fila* fila7;
-    fila* fila8;
-    fila* fila9;
-    fila0=inicializaFila();
-    fila1=inicializaFila();
-    fila2=inicializaFila();
-    fila3=inicializaFila();
-    fila4=inicializaFila();
-    fila5=inicializaFila();
-    fila6=inicializaFila();
-    fila7=inicializaFila();
-    fila8=inicializaFila();
-    fila9=inicializaFila();
-    fila* vetorRadix[10]={fila0,fila1,fila2,fila3,fila4,fila5,fila6,fila7,fila8,fila9};
 
 }
+
+//      Conversao int em str e depois str em int
+//char str[5];
+//int a = 2555;
+//
+//
+//sprintf(str,"%d",a);
+//
+//for(int i=0;i<strlen(str);++i)
+//printf("%c ",str[i]);
+//
+//a = atoi(str);
+//
+//printf("%d ",a);
+
+
 //   teste da fila.
 //
 //    fila* fila1;
